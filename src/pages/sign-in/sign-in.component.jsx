4b420a381cf2520460a639f4a7363811d0177d33
Ajay Singh <singh.ajay0124@gmail.com>
2021-05-27 16:41:28 -0700

    updated styles for sign in and sign up buttons/components and added sign in component and functionality

--- src/pages/sign-in/sign-in.component.jsx
+++ src/pages/sign-in/sign-in.component.jsx
@@ -5,7 +5,7 @@ import './sign-in.styles.scss';
 import FormInput from '../../components/form-input/form-input.component';
 import CustomButton from '../../components/custom-button/custom-button.component';
 
-import {signInWithGoogle} from '../../firebase/firebase.utils';
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils';
 
 class SignIn extends React.Component{
     constructor(props){
@@ -23,9 +23,17 @@ class SignIn extends React.Component{
         this.setState({[name]: value})
     }
 
-    handleSubmit = event => {
+    handleSubmit = async event => {
         event.preventDefault();
-        this.setState({ email: '', password: ''})
+
+        const {email, password} = this.state;
+        try{
+            await auth.signInWithEmailAndPassword(email,password);
+            this.setState({ email: '', password: ''});
+        } catch(err){
+            console.log(err);
+        }
+        
     }
 
     render() {
@@ -39,7 +47,7 @@ class SignIn extends React.Component{
                 <FormInput name="password" type="password" value={this.state.password} handleChange={this.handleChange} label="password" required />
                 <div className='buttons'>
                     <CustomButton type="submit">Sign In</CustomButton>
-                    <CustomButton onClick={signInWithGoogle} isGoogleSignIn>Sign In With Google</CustomButton>
+                    <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>Sign In With Google</CustomButton>
                 </div>
             </form>
             </div>
